895496e247c56a3f0bb21e9840864137eecc6ff9
Mohammed Saqlain <120318579+Sq1754@users.noreply.github.com>
2022-12-26 19:35:17 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q2 {
+	public static void main(String[]args) {
+		
+		// Q2  : print hollow rectangle
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Set values for Rows and Columns:");
+		int rows = sc.nextInt();
+		int columns = sc.nextInt();
+		
+		// outer loop
+		for(int i=1;i<=rows;i++) {
+			for(int j = 1; j<=columns;j++) {
+				// cell->(i,j)  visualize this as matrices
+				if(i==1||j==1||i==rows||j==columns) {
+					System.out.print("*");
+				}
+				else {System.out.print(" ");}
+			}
+			System.out.println();
+		}
+		
+	}
+}
